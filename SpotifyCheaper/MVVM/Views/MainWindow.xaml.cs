bc13690c38773e3ddc30e67ef2ff54a2f9e1d4f9
Dao An Phuoc <anphuocdao2410@gmail.com>
2024-11-04 13:24:10 +0700

    UI view list

--- SpotifyCheaper/MVVM/Views/MainWindow.xaml.cs
+++ SpotifyCheaper/MVVM/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using SpotifyCheaper.ViewModels;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,8 @@ namespace SpotifyCheaper
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
+
         }
     }
 }
