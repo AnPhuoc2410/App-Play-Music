14b163237b1e61fbe3727eff0e257524da1d0482
Dao An Phuoc <anphuocdao2410@gmail.com>
2024-11-08 14:49:40 +0700

    ListSong, Loop,Shuffle UI

--- SpotifyCheaper/MVVM/Models/Song.cs
+++ SpotifyCheaper/MVVM/Models/Song.cs
@@ -8,7 +8,10 @@ namespace SpotifyCheaper.MVVM.Models
 {
     public class Song
     {
-        public string Title {  get; set; }
-        public string Duration { get; set; }
+        public int TrackNumber { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Duration { get; set; } // Example: "3:57"
     }
+
 }
