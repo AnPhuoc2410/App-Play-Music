9f11f685e5de31a94b042bb0c8ec32a1f55fd214
DuongPKSE180524 <66048953+ReshTheDragon@users.noreply.github.com>
2024-11-08 16:49:32 +0700

    UI for video player

--- SpotifyCheaper/MVVM/Repositories/SpotifyRepository.cs
+++ SpotifyCheaper/MVVM/Repositories/SpotifyRepository.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +12,45 @@ namespace SpotifyCheaper.MVVM.Repositories
 {
     public class SpotifyRepository
     {
+        private readonly IConfiguration _configuration;
+        private string clientId;
+        private string clientSecret;
 
+        public SpotifyRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            clientId = _configuration["SpotifyID:ClientID"];
+            clientSecret = _configuration["SpotifyID:ClientSecret"];
+        }
+        public static async Task<string> GetSpotifyAccessToken(string clientId, string clientSecret)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                    Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}")));
+                var request = new HttpRequestMessage(HttpMethod.Post, "https://accounts.spotify.com/api/token");
+                request.Content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("grant_type", "client_credentials")
+                });
+                HttpResponseMessage response = await client.SendAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var tokenResponse = System.Text.Json.JsonSerializer.Deserialize<TokenResponse>(json);
+                    return tokenResponse?.AccessToken;
+                }
+                else
+                {
+                    Console.WriteLine("Error retrieving access token");
+                    return null;
+                }
+            }
+        }
+
+    }
+    public class TokenResponse
+    {
+        public string AccessToken { get; set; }
     }
 }
---
+++ SpotifyCheaper/MVVM/Views/VideoPlayerView.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpotifyCheaper.MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for VideoPlayerView.xaml
+    /// </summary>
+    public partial class VideoPlayerView : Page
+    {
+        public VideoPlayerView()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGoBack_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+
+            Window.GetWindow(this)?.Close();
+        }
+
+        private void BtnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnPlayPause_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnNext_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnRepeat_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnImport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
