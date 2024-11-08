901b8dc68858538b1edf2eea2f2d1fce4fadcc4f
DuongPKSE180524 <66048953+ReshTheDragon@users.noreply.github.com>
2024-11-08 23:12:05 +0700

    Video can now be Imported, play, pause is available

---
+++ SpotifyCheaper/MVVM/Models/Video.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyCheaper.MVVM.Models
+{
+    public class Video
+    {
+        public string TrackNumber { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public string FilePath { get; set; }
+
+    }
+}
--- SpotifyCheaper/MVVM/Views/VideoPlayerView.xaml.cs
+++ SpotifyCheaper/MVVM/Views/VideoPlayerView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using SpotifyCheaper.MVVM.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,16 +22,21 @@ namespace SpotifyCheaper.MVVM.Views
     /// </summary>
     public partial class VideoPlayerView : Window
     {
+
+        private MusicGetDataService _musicService;
+        private MediaPlayer _mediaPlayer;
+        private bool _isPlaying = false;
         public VideoPlayerView()
         {
             InitializeComponent();
         }
 
+        private string[] paths, files;
+
         private void BtnGoBack_Click(object sender, RoutedEventArgs e)
         {
             MainWindow main = new MainWindow();
             main.Show();
-
             Window.GetWindow(this)?.Close();
         }
 
@@ -40,7 +47,17 @@ namespace SpotifyCheaper.MVVM.Views
 
         private void BtnPlayPause_Click(object sender, RoutedEventArgs e)
         {
-
+            if (_isPlaying)
+            {
+                mediaElement.Pause();
+                PlayButton.Content = "⏯️";
+            }
+            else
+            {
+                mediaElement.Play();
+                PlayButton.Content = "⏸️";
+            }
+            _isPlaying = !_isPlaying;
         }
 
         private void BtnNext_Click(object sender, RoutedEventArgs e)
@@ -55,7 +72,17 @@ namespace SpotifyCheaper.MVVM.Views
 
         private void BtnImport_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Video Files|*.mp4;*.avi;*.mkv;*.wmv|All Files|*.*";
 
+            if (openFileDialog.ShowDialog()==true)
+            {
+                mediaElement.Source = new Uri(openFileDialog.FileName);
+                mediaElement.Play();
+                _isPlaying = true;
+                PlayButton.Content = "⏸️";
+            }
         }
     }
 }
