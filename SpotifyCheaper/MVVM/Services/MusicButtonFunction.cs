9d2d00e2600308f71df03c9f76af3ce53bcf15aa
soc day <socpro13579@gmail.com>
2024-11-12 16:15:19 +0700

    Ver 0.2.2: Search

--- SpotifyCheaper/MVVM/Services/MusicButtonFunction.cs
+++ SpotifyCheaper/MVVM/Services/MusicButtonFunction.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using SpotifyCheaper.MVVM.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SpotifyCheaper.MVVM.Services
@@ -9,9 +12,17 @@ namespace SpotifyCheaper.MVVM.Services
     public class MusicButtonFunction
     {
 
-        //public List<string> FindMusic (string inSeaching)
-        //{
+        public ObservableCollection<Song> FindMusic(string inSeaching, ObservableCollection<Song> songList)
+        {
+            ObservableCollection<Song> outListSong = new ObservableCollection<Song>();
+            string regexCondition = string.Empty;
+            for (int i = 0; i < songList.Count; i++)
+            {
+                Song song = songList[i];
+                if (song.Title.ToLower().Contains(inSeaching)) outListSong.Add(song);
+            }
+            return outListSong;
+        }
 
-        //}
     }
 }
